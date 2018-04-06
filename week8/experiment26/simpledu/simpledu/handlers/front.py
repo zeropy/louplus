9b648c022e6e6238689b800a8b888b8a7d0571a1
legion <1931330336@qq.com>
2018-04-06 10:36:09 +0800

    add experiment26

---
+++ week8/experiment26/simpledu/simpledu/handlers/front.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from flask import Flask, Blueprint, render_template
+from simpledu.models import Course
+from simpledu.forms import LoginForm, RegisterForm
+
+front = Blueprint('front',__name__)
+
+@front.route('/')
+def index():
+    courses = Course.query.all()
+    return  render_template('index.html',courses=courses)
+
+@front.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html',form=form)
+
+@front.route('/register')
+def register():
+    form = RegisterForm()
+    return render_template('register.html',form=form)
