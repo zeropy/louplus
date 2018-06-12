e03ceb37f405d6ab3f6042d976d55ce3a53a7808
legion <1931330336@qq.com>
2018-06-12 21:47:41 +0800

    bugfix

--- week13/challenge48/cache.py
+++ week13/challenge48/cache.py
@@ -11,11 +11,20 @@ class RedisCache(object):
         def decorator(func):
             def wrapper(*args, **kwargs):
                 result = self._redis.get(func.__name__)
-                if result: return json.loads(result)
+                if result: return json.loads(result.decode('utf-8'))
                 result = func(*args, **kwargs)
-                result = json.dumps(result)
-                self._redis.set(func.__name__, result)
+                save = json.dumps(result)
+                self._redis.set(func.__name__, save)
                 self._redis.expire(func.__name__, timeout)
                 return result
             return wrapper
         return decorator
+
+if __name__ == '__main__':
+    r = redis.StrictRedis(host='localhost',port=6379,db=0)
+    cache = RedisCache(r)
+    @cache.cache(timeout=10)
+    def test():
+        return {'a':1}
+    result = test()
+    print(result)
