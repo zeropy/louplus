9df6c82dafb73591ec862df54e336bcfa33f3eee
legion <1931330336@qq.com>
2018-06-12 20:37:16 +0800

    challenge48 init

---
+++ week13/challenge48/rmon/rmon/views/urls.py
@@ -0,0 +1,20 @@
+""" rmon.views.urls
+
+定义了所有 API 对应的 URL
+"""
+from flask import Blueprint
+
+from rmon.views.index import IndexView
+from rmon.views.server import ServerList
+from rmon.views.server import ServerDetail
+from rmon.views.server import ServerMetrics
+
+api = Blueprint('api', __name__)
+
+api.add_url_rule('/', view_func=IndexView.as_view('index'))
+api.add_url_rule('/servers/',
+                 view_func=ServerList.as_view('server_list'))
+api.add_url_rule('/servers/<int:object_id>',
+                 view_func=ServerDetail.as_view('server_detail'))
+api.add_url_rule('/server/<int:object_id>/metrics',
+                 view_func=ServerMetrics.as_view('server_metrics'))
