ea0c71b8ffe65232717334471aed50f6f0124542
legion <1931330336@qq.com>
2018-06-14 07:41:57 +0800

    challenge49

---
+++ week13/challenge49/create_server.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+
+import requests
+import json
+
+def create_server(name, host):
+    url = 'http://127.0.0.1:5000/servers/'
+    headers = {'Content-Type': 'application/json'}
+    data = {'name':name, 'host':host}
+    r = requests.post(url,data=json.dumps(data), headers=headers)
+    print(r.content)
+    return r.json()
+
+if __name__ == '__main__':
+    create_server('test','127.0.0.1')
